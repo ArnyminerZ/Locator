441e0c1cc1ef27e528758d4fae6480a4df0abbeb
Arnau Mora <arnyminer.z@gmail.com>
2023-02-27 12:01:08 +0100

    Updated dependencies
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,11 +32,11 @@ repositories {
 dependencies {
     /* Example Dependency */
     /* Depend on the android gradle plugin, since we want to access it in our plugin */
-    implementation("com.android.tools.build:gradle:7.4.0")
+    implementation("com.android.tools.build:gradle:7.4.1")
 
     /* Example Dependency */
     /* Depend on the kotlin plugin, since we want to access it in our plugin */
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
 
     /* Depend on the default Gradle API's since we want to build a custom plugin */
     implementation(gradleApi())
